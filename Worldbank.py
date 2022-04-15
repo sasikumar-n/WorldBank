f802efa1bf5dd1b6a30b34eff2fd8c200fa3666c
SASIKUMAR N <100081410+sasikumar-n@users.noreply.github.com>
2022-04-15 22:00:03 +0530

    Update Worldbank.py

--- Worldbank.py
+++ Worldbank.py
@@ -4,6 +4,11 @@ import altair as alt
 import streamlit as st
 
 st.title('worldBankIndia')
-india = pd.read_csv('World_Bank_India.csv', skiprows=4)
-st.write(india)
-st.write(india.index)
+df = pd.read_csv('World_Bank_India.csv', skiprows=4)
+df.set_index("Indicator Name", inplace=True)
+df1 = df.loc[['Physicians (per 1,000 people)', 'Number of deaths ages 5-9 years','People using safely managed drinking water services, rural (% of rural population)'],['2010',	'2011',	'2012',	'2013',	'2014',	'2015',	'2016',	'2017',	'2018'
+]]
+tdf1 = df1.transpose()
+st.write(tdf1)
+
+
